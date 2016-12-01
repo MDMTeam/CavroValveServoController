1e7a860f0af0918b2622f59d5500e85b6613e463
BuildTools <unconfigured@null.spigotmc.org>
2016-12-01 21:56:11 +0100

    Re-did everything. Repaired button location & position. Help me plz with the Auto-Scrolling function.

---
+++ src/InitJThings.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.text.DefaultCaret;
+import java.awt.*;
+
+public class InitJThings {
+
+    JFrame f = new JFrame();
+    JButton buttonPlus = new JButton("Hello");
+
+    JTextArea commandLine = new JTextArea("Status: Initiated!");
+    Border border = BorderFactory.createLineBorder(Color.BLACK);
+
+
+}
