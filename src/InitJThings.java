e436cbeb8dd3f6010f83b85926b44bbcd707a302
BuildTools <unconfigured@null.spigotmc.org>
2016-12-04 19:48:47 +0100

    Re-did everything. Repaired button location & position. Help me plz with the Auto-Scrolling function.

--- src/InitJThings.java
+++ src/InitJThings.java
@@ -5,12 +5,13 @@ import java.awt.*;
 public class InitJThings {
 
     JFrame f = new JFrame();
-    JButton buttonPlus = new JButton("Hello");
+    JButton buttonPlus = new JButton("Write");
     JButton buttonExit = new JButton("Exit...");
 
     JTextArea status = new JTextArea("Status: INITIATED");
     JScrollPane commandLine = new JScrollPane(status, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     Border border = BorderFactory.createLineBorder(Color.BLACK);
+
 
 
 }
